1f061971497dde5b560a0820251e032d3f743c24
Edwin Lopez <screaber@hotmail.com>
2012-04-09 10:40:55 -0500

    Funcionalidad cuando numero es 0

--- edwinlopez/nromano.rb
+++ edwinlopez/nromano.rb
@@ -1,15 +1,14 @@
 class Nromano
   SYMBOLS = [["C",100],["L",50],["X",10],["V",5],["I",1]]
   def convertir(numeronatural)
+  	if numeronatural == 0
+			raise "No se puede convertir el numero 0"
+    end
   	result = ""
   	SYMBOLS.each do | romano, numero |
   		while numeronatural >=  numero
-        if numeronatural>=numero
-        	result += romano
-        else
-            result = romano	
-        end	      
-        numeronatural -= numero
+        	result += romano     
+            numeronatural -= numero
     	end
     end  
 	return result
--- edwinlopez/nromano_spec.rb
+++ edwinlopez/nromano_spec.rb
@@ -2,6 +2,12 @@ require './nromano.rb'
 
 describe Nromano do
 
+   it "no debe ser 0" do
+    expect {
+        @romano.convertir_romano(0)
+      }.should raise_exception
+   end
+
 context "convertir " do
     it "el 1 en I" do
     @nromano = Nromano.new()   
@@ -30,12 +36,21 @@ context "convertir " do
      @nromano = Nromano.new()   
      @nromano.convertir(2).should == "II"
     end  
-  end
-
-  context "Agregar un valor " do
+  
     it "el 3 debe convertirse en III" do
      @nromano = Nromano.new()   
      @nromano.convertir(3).should == "III"
+    end 
+
+    it "16 debe convertirse en XVI" do
+     @nromano = Nromano.new()   
+     @nromano.convertir(16).should == "XVI"
     end  
+  end
+
+   context "Disminuir un valor " do
+    it "4 debe convertirse en IV" do
+     @nromano.convertir(4).should == "IV"
+    end     
   end
 end
